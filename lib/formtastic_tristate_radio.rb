effdca8701a0a8c6584d34b44e777d4a311570ee
Sergey Pedan <sergey.pedan@gmail.com>
2021-11-02 21:15:57 +0700

    Uses relative require

--- lib/formtastic_tristate_radio.rb
+++ lib/formtastic_tristate_radio.rb
@@ -1,7 +1,7 @@
 require "formtastic_tristate_radio/version"
 require "formtastic_tristate_radio/engine"
 
-require "app/inputs/tristate_radio_input"
+require_relative "../app/inputs/tristate_radio_input"
 
 module FormtasticTristateRadio
   # Your code goes here...
