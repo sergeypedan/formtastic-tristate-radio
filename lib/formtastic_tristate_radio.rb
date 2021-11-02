f32017d0b30125b559167ebd54f06175d03129c9
Sergey Pedan <sergey.pedan@gmail.com>
2021-11-02 22:21:52 +0700

    Adds nil

--- lib/formtastic_tristate_radio.rb
+++ lib/formtastic_tristate_radio.rb
@@ -1,6 +1,7 @@
 require "formtastic_tristate_radio/version"
 require "formtastic_tristate_radio/engine"
 
+require_relative "../app/models/activerecord_base"
 # require_relative "../app/inputs/tristate_radio_input"
 
 module FormtasticTristateRadio
