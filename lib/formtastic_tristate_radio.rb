f175a88894c040ff9d9105c4e40fdad265b923cc
Sergey Pedan <sergey.pedan@gmail.com>
2021-11-05 00:50:25 +0700

    Moves error into a separate file

--- lib/formtastic_tristate_radio.rb
+++ lib/formtastic_tristate_radio.rb
@@ -1,9 +1,7 @@
 require_relative "formtastic_tristate_radio/version"
+require_relative "formtastic_tristate_radio/missing_translation_error"
 require_relative "formtastic_tristate_radio/engine"
 require_relative "../app/models/active_record/base"
 
 module FormtasticTristateRadio
-
-  class MissingTranslationError < StandardError; end
-
 end
---
+++ lib/formtastic_tristate_radio/missing_translation_error.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+module FormtasticTristateRadio
+  class MissingTranslationError < StandardError
+  end
+end
