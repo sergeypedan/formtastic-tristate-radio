72951c1f3f315061ffea19091ba4d9b26338eacb
Sergey Pedan <sergey.pedan@gmail.com>
2021-11-04 14:43:10 +0700

    Adds comments on module

--- lib/formtastic_tristate_radio/engine.rb
+++ lib/formtastic_tristate_radio/engine.rb
@@ -1,4 +1,11 @@
+# frozen_string_literal: true
+
 module FormtasticTristateRadio
+
+  # This is standard Rails way to autoload gem’s contents dynamically as an “engine”
+  # https://guides.rubyonrails.org/engines.html
+  #
   class Engine < ::Rails::Engine
   end
+
 end
